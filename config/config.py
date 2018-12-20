0d96e31adbe3d0cc2545803497283e72bcd4c5f6
Colman <colman0423@gmail.com>
2018-12-20 11:40:04 +0800

    remove config

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 google_key = {
-    'apiKey': 'AIzaSyAD4qydu6OTwsstN7LhbHCnJjcvF2Fjovs',
-    'clientId': '8372100595-q7sv932tbof1ome2td9ktt0q4c02hvjd.apps.googleusercontent.com',
-    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    'scope': "https://www.googleapis.com/auth/spreadsheets.readonly",
+    'apiKey': '',
+    'clientId': '',
+    'discoveryDocs': [],
+    'scope': "",
 }
-driver_path = "/Applications/chromedriver"
+driver_path = ""
